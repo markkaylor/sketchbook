c527b8a9ed09a52f15a7e86396661436e410c6f8
markkaylor <mkaylor.guitar@gmail.com>
2020-05-21 22:01:27 +0200

    Fetch data from youtube on build

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,42 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const axios = require("axios")
+const ypi = require("youtube-playlist-info")
 
-// You can delete this file if you're not using it
+require("dotenv").config({
+  path: `.env`,
+})
+
+exports.sourceNodes = async ({
+  actions: { createNode },
+  createContentDigest,
+}) => {
+  // Get all playlist id's form youtube
+  const result = await axios.get(
+    `https://www.googleapis.com/youtube/v3/playlists?part=id%2C%20snippet&channelId=${process.env.YOUTUBE_CHANNEL_ID}&key=${process.env.YOUTUBE_KEY}`
+  )
+
+  const playlistInfo = result.data.items.map(async playlist => {
+    let info = await ypi(process.env.YOUTUBE_KEY, playlist.id)
+    return Object.assign({
+      playlistTitle: playlist.snippet.title,
+      videos: info,
+    })
+  })
+
+  const allYoutubePlaylist = await Promise.all(playlistInfo)
+
+  // For each playlist id get playlist info
+  allYoutubePlaylist.forEach(playlist => {
+    createNode({
+      playlist,
+      id: playlist.playlistTitle,
+      parent: null,
+      children: [],
+      internal: {
+        type: `youtubePlaylist`,
+        contentDigest: createContentDigest(playlist),
+      },
+    })
+  })
+}
+
+// Create a page for each playlist
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,33 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import { graphql, useStaticQuery } from "gatsby"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const indexData = useStaticQuery(graphql`
+    query {
+      allYoutubePlaylist {
+        edges {
+          node {
+            playlist {
+              playlistTitle
+            }
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {indexData.allYoutubePlaylist.edges.map(({ node }) => (
+        <h1>
+          <Link to="/page-2/">{node.playlist.playlistTitle}</Link>
+        </h1>
+      ))}
+    </Layout>
+  )
+}
 
 export default IndexPage
